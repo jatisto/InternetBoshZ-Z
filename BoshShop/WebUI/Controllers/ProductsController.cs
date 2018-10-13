24f5373267af1c3eee6ea8053e5c62ece30f0f0b
EugeneTM <j.atisto@gmail.com>
2018-10-14 01:17:47 +0600

    not work lesson 2

---
+++ BoshShop/WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Castle.Components.DictionaryAdapter.Xml;
+using System.IO;
+using Domain.Abstract;
+
+namespace WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductsController()
+        {
+            
+        }
+        public ProductsController(IProductRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
---
+++ BoshShop/WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Domain.Abstract;
+using Domain.Concrete;
+using Domain.Entities;
+using Moq;
+using Ninject;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            AddBinding();
+        }
+
+        private void AddBinding()
+        {
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
